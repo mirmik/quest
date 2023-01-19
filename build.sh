b5378795bbd9e985a59fd9ffae6dfaee0c6866b9
mirmik <mirmikns@yandex.ru>
2023-01-20 02:23:35 +0300

    'a'

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ NDK_HOME=/home/mirmik/Android/Sdk/ndk/22.1.7171670
 OVR_HOME=/home/mirmik/src/ovr_sdk_mobile_1.50.0
 ANDROID_HOME=/home/mirmik/Android/Sdk
 #JAVACPATH=/home/mirmik/soft/android-studio/jre/bin/javac
-JAVACPATH=javac
-DEXPATH=$ANDROID_HOME/build-tools/28.0.3
+JAVACPATH=/usr/lib/jvm/java-8-openjdk-amd64/bin/javac
+DEXPATH=$ANDROID_HOME/build-tools/30.0.3
 COMPILERPATH=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
 
 rm -rf build
@@ -22,54 +22,6 @@ $JAVACPATH\
 $DEXPATH/dx --dex --output classes.dex .
 mkdir -p lib/arm64-v8a
 pushd lib/arm64-v8a > /dev/null
-$COMPILERPATH/aarch64-linux-android$ANDROID_NUM-clang\
-    -march=armv8-a\
-    -fPIC\
-    -shared \
-    -std=c++17\
-    -I ~/project/rabbit\
-    -I ~/project/nos\
-    -I ~/project/igris\
-    -I ~/project/ralgo\
-    -I ~/project/crow\
-    -I $NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/\
-    -I $OVR_HOME/VrApi/Include\
-    -L $NDK_HOME/platforms/$ANDROID_PLATFORM/arch-arm64/usr/lib\
-    -L $OVR_HOME/VrApi/Libs/Android/arm64-v8a/Debug\
-    -L /home/mirmik/Downloads/OpenCV-android-sdk/sdk/native/staticlibs/arm64-v8a/ \
-    -L /home/mirmik/Downloads/OpenCV-android-sdk/sdk/native/3rdparty/libs/arm64-v8a \
-    -o libmain_cxx.so \
-    -Wl,--start-group \
-    -landroid\
-    -llog\
-    -lvrapi\
-    -lGLESv3\
-    -lEGL\
-    -DWITH_OPENEXR=OFF -DBUILD_OPENEXR=OFF \
-   ../../../src/main/cpp/*.cpp  \
-   ~/project/rabbit/rabbit/opengl/drawer.cpp \
-   ~/project/rabbit/rabbit/opengl/opengl_shader_program.cpp \
-   ~/project/rabbit/rabbit/opengl/shader_collection.cpp \
-   ~/project/rabbit/rabbit/mesh.cpp \
-   ~/project/rabbit/rabbit/font/naive.cpp \
-   ~/project/crow/crow/pubsub/pubsub.cpp \
-   ~/project/crow/crow/src/tower.cpp \
-   ~/project/crow/crow/src/threads-posix.cpp \
-   ~/project/crow/crow/src/gateway.cpp \
-   ~/project/crow/crow/src/address.cpp \
-   ~/project/crow/crow/src/packet.cpp \
-   ~/project/crow/crow/src/packet_ptr.cpp \
-   ~/project/crow/crow/gates/udpgate.cpp \
-   ~/project/crow/crow/proto/node.cpp \
-   ~/project/crow/crow/proto/node-sync.cpp \
-   ~/project/crow/crow/src/hostaddr.cpp \
-   ~/project/igris/igris/sync/syslock_mutex.cpp \
-   ~/project/igris/igris/osutil/src/posix.cpp \
-   ~/project/igris/igris/util/string.cpp \
-   ~/project/igris/igris/osinter/wait-linux.cpp \
-   ~/project/igris/igris/string/replace.cpp \
-   /home/mirmik/Android/Sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/$ANDROID_NUM/libc++.a \
-    -Wl,--end-group 
 
 $COMPILERPATH/aarch64-linux-android$ANDROID_NUM-clang\
     -march=armv8-a\
@@ -80,12 +32,8 @@ $COMPILERPATH/aarch64-linux-android$ANDROID_NUM-clang\
     -I ~/project/nos\
     -I ~/project/igris\
     -I ~/project/ralgo\
-    -I ~/project/crow\
     -I $NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/\
     -I $OVR_HOME/VrApi/Include\
-    -I ~/Downloads/opencv-4.5.2/modules/imgcodecs/include/ \
-    -I ~/Downloads/opencv-4.5.2/modules/core/include/ \
-    -I ~/Downloads/opencv-4.5.2/include/ \
     -L $NDK_HOME/platforms/$ANDROID_PLATFORM/arch-arm64/usr/lib\
     -L $OVR_HOME/VrApi/Libs/Android/arm64-v8a/Debug\
     -L /home/mirmik/Downloads/OpenCV-android-sdk/sdk/native/staticlibs/arm64-v8a/ \
@@ -104,6 +52,61 @@ $COMPILERPATH/aarch64-linux-android$ANDROID_NUM-clang\
    ~/project/igris/igris/dprint/dprint_stdout.c \
    ~/project/igris/igris/string/replace_substrings.c \
    ~/project/igris/igris/string/memmem.c \
+   /home/mirmik/Android/Sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/$ANDROID_NUM/libc++.a \
+    -Wl,--end-group 
+
+$COMPILERPATH/aarch64-linux-android$ANDROID_NUM-clang\
+    -march=armv8-a\
+    -fPIC\
+    -shared \
+    -std=c++17\
+    -I ~/project/rabbit\
+    -I ~/project/nos\
+    -I ~/project/igris\
+    -I ~/project/ralgo\
+    -I $NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/\
+    -I $OVR_HOME/VrApi/Include\
+    -L $NDK_HOME/platforms/$ANDROID_PLATFORM/arch-arm64/usr/lib\
+    -L $OVR_HOME/VrApi/Libs/Android/arm64-v8a/Debug\
+    -L . \
+    -o libmain.so \
+    -Wl,--start-group \
+    -lEGL\
+    -landroid\
+    -llog\
+    -lvrapi\
+    -lGLESv3\
+    -lmain_cc\
+    -DWITH_OPENEXR=OFF -DBUILD_OPENEXR=OFF \
+   ../../../src/main/cpp/*.cpp  \
+   ~/project/rabbit/rabbit/opengl/drawer.cpp \
+   ~/project/rabbit/rabbit/opengl/opengl_shader_program.cpp \
+   ~/project/rabbit/rabbit/opengl/shader_collection.cpp \
+   ~/project/rabbit/rabbit/mesh.cpp \
+   ~/project/rabbit/rabbit/font/naive.cpp \
+   ~/project/igris/igris/sync/syslock_mutex.cpp \
+   ~/project/igris/igris/osutil/src/posix.cpp \
+   ~/project/igris/igris/util/string.cpp \
+   ~/project/igris/igris/osinter/wait-linux.cpp \
+   ~/project/igris/igris/string/replace.cpp \
+    ~/project/nos/nos/trent/json.cpp \
+    ~/project/nos/nos/print/print.cpp \
+    ~/project/nos/nos/print/stdtype.cpp \
+    ~/project/nos/nos/fprint/fprint.cpp \
+    ~/project/nos/nos/fprint/fstdtype.cpp \
+    ~/project/nos/nos/util/nos_numconvert.cpp \
+    ~/project/nos/nos/io/ostream.cpp \
+    ~/project/nos/nos/io/istream.cpp \
+    ~/project/nos/nos/input/input.cpp \
+    ~/project/nos/nos/io/impls/current_ostream_stdout.cpp \
+    ~/project/nos/nos/io/stdfile.cpp \
+    ~/project/nos/nos/io/file.cpp \
+    ~/project/nos/nos/inet/tcp_client.cpp \
+    ~/project/nos/nos/inet/tcp_socket.cpp \
+    ~/project/nos/nos/inet/udp_socket.cpp \
+    ~/project/nos/nos/inet/common.cpp \
+    ~/project/nos/nos/util/string.cpp \
+    ~/project/nos/nos/util/osutil_unix.cpp \
    /home/mirmik/Android/Sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/$ANDROID_NUM/libc++.a \
     -Wl,--end-group 
 
@@ -131,7 +134,7 @@ aapt\
 	-f
 aapt add hello_quest.apk classes.dex
 aapt add hello_quest.apk lib/arm64-v8a/libmain_cc.so
-aapt add hello_quest.apk lib/arm64-v8a/libmain_cxx.so
+aapt add hello_quest.apk lib/arm64-v8a/libmain.so
 aapt add hello_quest.apk lib/arm64-v8a/libvrapi.so
 #aapt add hello_quest.apk /home/mirmik/Downloads/OpenCV-android-sdk/sdk/native/staticlibs/arm64-v8a/libopencv_core.a
 #aapt add hello_quest.apk /home/mirmik/Downloads/OpenCV-android-sdk/sdk/native/staticlibs/arm64-v8a/libopencv_imgcodecs.a
